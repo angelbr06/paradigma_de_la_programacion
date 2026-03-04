08451e72027c411bd1d8180e426f84154a6d037b
angelbr06 <garfielini32@gmail.com>
2026-03-04 15:32:06 -0500

    funciones.js

---
+++ funciones.js
@@ -0,0 +1,30 @@
+
+let resultado = 0;
+function sumarprecios( a , b)  {
+    return resultado =resultado + a + b;
+};
+
+console.log(sumarprecios(4, 6));
+console.log(sumarprecios(2,3));
+console.log(sumarprecios(5, 8));
+
+let Var= ""
+function Oracion( a ){
+    return Var = Var + a ;
+};
+
+console.log(Oracion( "enter" ));
+console.log(Oracion( "game" ));
+console.log(Oracion(" ,"));
+console.log(Oracion( "select game "))
+
+
+function Multiplicar(a) {
+    return a * a;
+};
+
+function SumaDelaMultiplicacion(num1, num2) {
+    return Multiplicar(num1) + Multiplicar(num2);
+};
+
+console.log ( SumaDelaMultiplicacion(3, 4) );
